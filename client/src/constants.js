87afe7172686896e8274598b90a7f60624c67bf9
Marco Echevestre <marcoaminotto@hotmail.com>
2021-03-23 16:29:47 +0100

    Create interface of client side using blessed library

---
+++ client/src/constants.js
@@ -0,0 +1,11 @@
+export const constants = {
+  events: {
+    app: {
+      MESSAGE_SENT: 'message:sent',
+      MESSAGE_RECEIVERD: 'message:received',
+      LOG_UPDATED: 'log:updated',
+      STATUS_UPDATED: 'status:updated',
+    },
+    socket: {},
+  },
+};
