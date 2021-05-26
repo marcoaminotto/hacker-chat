fe77a6e041475e3b57494679199d7c8fac71bf50
Marco Echevestre <devminotto@gmail.com>
2021-05-26 12:50:13 +0200

    Implement sockets at client side

--- client/index.js
+++ client/index.js
@@ -1,7 +1,14 @@
 import Events from 'events';
+import cliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 import TerminalController from './src/terminalController.js';
 
+const [nodePath, filePath, ...commands] = process.argv;
+const config = cliConfig.parseArguments(commands);
+
 const componentEmitter = new Events();
+const socketClient = new SocketClient(config);
+await socketClient.initialize();
 
 const controller = new TerminalController();
 
