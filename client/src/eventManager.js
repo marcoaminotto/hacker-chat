dd62eb0c5a6aaefcd6fe33727262ba8102a37e36
Marco Echevestre <devminotto@gmail.com>
2021-05-28 11:05:14 +0200

    Build a event control of users joining the chat

---
+++ client/src/eventManager.js
@@ -0,0 +1,46 @@
+import { constants } from './constants.js';
+
+export default class EventManager {
+  #allUsers = new Map();
+  constructor({ componentEmitter, socketClient }) {
+    this.componentEmitter = componentEmitter;
+    this.socketClient = socketClient;
+  }
+
+  joinRoomAndWaitForMessages(data) {
+    this.socketClient.sendMessage(constants.events.socket.JOIN_ROOM, data);
+
+    this.componentEmitter.on(constants.events.app.MESSAGE_SENT, (msg) => {
+      this.socketClient.sendMessage(constants.events.socket.MESSAGE, msg);
+    });
+  }
+
+  updateUsers(users) {
+    const connectedUsers = users;
+    connectedUsers.forEach(({ id, userName }) =>
+      this.#allUsers.set(id, userName)
+    );
+    this.#updateUsersComponent(this.#allUsers);
+  }
+
+  newUserConnected(message) {
+    const user = message;
+    this.#allUsers.set(user.id, user.userName);
+    this.#updateUsersComponent();
+  }
+
+  #updateUsersComponent() {
+    this.componentEmitter.emit(
+      constants.events.app.STATUS_UPDATED,
+      Array.from(this.#allUsers.values())
+    );
+  }
+
+  getEvents() {
+    const functions = Reflect.ownKeys(EventManager.prototype)
+      .filter((fn) => fn !== 'contructor')
+      .map((name) => [name, this[name].bind(this)]);
+
+    return new Map(functions);
+  }
+}
--- server/src/controller.js
+++ server/src/controller.js
@@ -1,5 +1,8 @@
+import { constants } from './constants.js';
+
 export default class Controller {
   #users = new Map();
+  #rooms = new Map();
 
   constructor({ socketServer }) {
     this.socketServer = socketServer;
@@ -16,15 +19,70 @@ export default class Controller {
     socket.on('end', this.#onSocketClosed(id));
   }
 
+  async joinRoom(socketId, data) {
+    const userData = data;
+    console.log(`${userData.userName} joinde! ${[socketId]}`);
+    const { roomId } = userData;
+    const user = this.#updateGlobalUserData(socketId, userData);
+    const users = this.#joinUserOnRoom(roomId, user);
+
+    const currentUsers = Array.from(users.values()).map(({ id, userName }) => ({
+      userName,
+      id,
+    }));
+
+    this.socketServer.sendMessage(
+      user.socket,
+      constants.events.UPDATE_USERS,
+      currentUsers
+    );
+
+    this.broadCast({
+      socketId,
+      roomId,
+      message: { id: socketId, userName: userData.userName },
+      event: constants.events.NEW_USER_CONNECTED,
+    });
+  }
+
+  broadCast({
+    socketId,
+    roomId,
+    event,
+    message,
+    includeCurrentSocket = false,
+  }) {
+    const usersOnRoom = this.#rooms.get(roomId);
+
+    for (const [key, user] of usersOnRoom) {
+      if (!includeCurrentSocket && key === socketId) continue;
+
+      this.socketServer.sendMessage(user.socket, event, message);
+    }
+  }
+
+  #joinUserOnRoom(roomId, user) {
+    const usersOnRoom = this.#rooms.get(roomId) ?? new Map();
+    usersOnRoom.set(user.id, user);
+    this.#rooms.set(roomId, usersOnRoom);
+
+    return usersOnRoom;
+  }
+
   #onSocketData(id) {
     return (data) => {
-      console.log('onSocketData: ', data.toString());
+      try {
+        const { event, message } = JSON.parse(data);
+        this[event](id, message);
+      } catch (error) {
+        console.error(`wrong event format`, data.toString());
+      }
     };
   }
 
   #onSocketClosed(id) {
     return (data) => {
-      console.log('onSocketClosed: ', data.toString());
+      console.log('onSocketClosed: ', id);
     };
   }
 
